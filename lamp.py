1cdee58e2b32880d787b3604cf45e4582d398e77
KajalTomar <52635061+KajalTomar@users.noreply.github.com>
2020-12-09 16:55:28 -0600

    Update lamp.py
    took out brightness functions and added changeColour function

--- lamp.py
+++ lamp.py
@@ -17,13 +17,6 @@ TURN_OFF_TIME = "22:00:00" # military time
 # Ryan has 14 hours (50,400,000 milliseconds) to press the button
 # if we want the lamp to be on from 8am to 10 pm
 TIME_TO_PRESS = 50400000
-
-DC_CONSTANT = 4.62606500918  # constant used to calculate duty cycle
-
-# from 1 to 3
-MIN_BRIGHTNESS = 0
-MAX_BRIGHTNESS = 3
-
 # -----------------------
 
 # for the GPIO pins (using BOARD)
@@ -90,51 +83,26 @@ def writeToStreak(strk):
 	newStreakFile.write(strk);
 	newStreakFile.close()
 
-#------------------------------------------------------------
-# setBrightness
-#
-# PURPOSE: sets the initial (before button is pressed)
-# brightness for the lamp.
-# INPUT: the current streak (int)
-# OUTPUT: the brightness level (int)
-#------------------------------------------------------------
-def setBrightness(strk):
-	bright =  strk
-	if(streak > MAX_BRIGHTNESS):
-		bright = MAX_BRIGHTNESS
-	elif (streak < MIN_BRIGHTNESS):
-		bright = MIN_BRIGHTNESS
-	return bright
-
-#------------------------------------------------------------
-# updateBrightness
-#
-# PURPOSE: increases the brightness because the button got
-# pressed!
-# INPUT: the current brightness (int).
-# OUTPUT: the new updated brightness.
-#-------------------------------------------------------------
-def updateBrightness(currentBright):
-	bright = currentBright + 1;
-
-	if (bright > MAX_BRIGHTNESS):
-		bright = MAX_BRIGHTNESS
-	elif(bright < MIN_BRIGHTNESS):
-		bright = MIN_BRIGHTNESS
-
-	return bright
-
-#-------------------------------------------------------------
-# calculateDutyCycle
-#
-# PURPOSE: Calculates the duty cycle for the PWM.
-# INPUT: brightness (int) (used for calculation)
-# OUTPUT: dc = duty cycle (int)
-#--------------------------------------------------------------
-def calculateDutyCycle(bright):
-	dc = DC_CONSTANT**(bright)-1
-	return dc
-
+def changeColour(color):
+	red.stop()
+	green.stop()
+	blue.stop()
+	
+	if(color=='yellow'):
+		# yellow
+		red.start(100)
+		green.start(20)
+	elif(color=='orange'):
+		#orange
+		red.start(100)
+		green.start(2)
+	elif(color=='pink'):
+		#pink
+		red.start(100)
+		blue.start(1)
+	elif(color=='red'):
+		#red
+		red.start(70)
 #--------------------------------------------------------------
 # whatTime
 #
@@ -160,43 +128,28 @@ writeToStat(currentDate)
 streak = readStreak()
 print("streak: "+ str(streak))
 
-# set up the brightness amount based on current streak
-brightness =  setBrightness(streak)
-
-# calculate PWM and turn on LED!
-dutyCycle = calculateDutyCycle(brightness)
-
+# set the lamp to initial colors
 red  = GPIO.PWM(R, 1000)
 blue  = GPIO.PWM(B, 1000)
 green = GPIO.PWM(G, 1000) 
 
-print("dutycycle: "+ str(dutyCycle))
-
 if (streak == 1):
-	# yellow
-	red.start(100)
-	green.start(20)
-	blue.start(0)
+	changeColour('yellow')
 elif (streak == 2):
-	#orange
-	red.start(100)
-	green.start(2)
-	blue.start(0)
+	changeColour('orange')
 elif(streak >= 3):
-	#red
-	red.start(70)
-	green.start(0)
-	blue.start(0)
+	changeColour('red')
+
 # just wait for Ryan to press the button for 14 hours
 button = GPIO.wait_for_edge(BUTTON, GPIO.FALLING, timeout = TIME_TO_PRESS)
 if button is None:
-	#WHEN THE BUTTON IS *NOT* PRESSED
+	# IF THE BUTTON IS *NOT* PRESSED
 	writeToStat('|0|00:00:00\n')
 	writeToStreak('0')
 	print("The button was not pressed.")
 	red.stop()
 	blue.stop()
-	# green.stop()
+	green.stop()
 
 	GPIO.cleanup()
 else:
@@ -205,47 +158,27 @@ else:
 
 	# update these values to reflect button pressed
 	streak += 1
-
-	brightness =  updateBrightness(brightness)
-	dutyCycle = calculateDutyCycle(brightness)
-
+	
 	print("Button pressed.")
 	print("New streak: " + str(streak))
+	
 	# update the files that Ryan pressed the button!
 	writeToStat('|1|'+str(timePressed)+'\n')
 	writeToStreak(str(streak))
 
-	# make the lamp brighter!
+	# update the color of the lamp!
 	# keep the lamp on unless it is TURN_OFF_TIME or I ctrl+C
-
-	print("dutyCycle: " + str(dutyCycle))
 	try:
-		red.stop()
-		green.stop()
-		blue.stop()
-
 		if(streak >= 3):
-		  	#pink
-			red.start(100)
-			green.start(0)
-			blue.start(1)
+		  	changeColour('pink')
 			time.sleep(1)
 		while(whatTime() != TURN_OFF_TIME):
 			if (streak == 1):
-        			# yellow
-        			red.start(100)
-        			green.start(30)
-        			blue.start(0)
+        			changeColour('yellow')
 			elif (streak == 2):
-        			#orange
-        			red.start(100)
-	        		green.start(5)
-        			blue.start(0)
+        			changeColour('orange')
 			elif(streak >= 3):
-				#red
-				red.ChangeDutyCycle(70)
-				green.ChangeDutyCycle(0)
-				blue.ChangeDutyCycle(0)
+				changeColour('red')
 			time.sleep(.3)
 	except KeyboardInterrupt:
 		pass
